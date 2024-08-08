f2da3183a5a8d8aeaf7ce2c4a4c5cbb879d25569
Ankur4600 <tinkushrivastav78@gmail.com>
2024-08-08 20:22:19 +0530

    small projects using html,css and javaScript

---
+++ script/cricketGame.js
@@ -0,0 +1,86 @@
+// let randomNumber=Math.random()*3;
+// let computerChoise;    
+function bat(){
+    let randomNumber=Math.random()*3;
+    let computerChoise;  
+    if(randomNumber>0 && randomNumber<=1){
+        computerChoise='bat';
+    }
+    else if(randomNumber>1 && randomNumber<=2){
+        computerChoise='ball';
+    }
+    else if( randomNumber>2 && randomNumber<=3){
+        computerChoise='stump';
+    }
+// }
+// function bat(){
+    
+    let result;
+    if(computerChoise==='bat'){
+        result=`it's a tie`;
+    }
+    else if(computerChoise==='ball'){
+        result='you won';
+    }
+    else if( computerChoise==='stump'){
+        result='computer won';
+    }
+
+    alert(`computer choice is ${computerChoise}. your choice is bat and ${result}`);
+}
+function ball(){
+    let randomNumber=Math.random()*3;
+    let computerChoise;  
+    if(randomNumber>0 && randomNumber<=1){
+        computerChoise='bat';
+    }
+    else if(randomNumber>1 && randomNumber<=2){
+        computerChoise='ball';
+    }
+    else if( randomNumber>2 && randomNumber<=3){
+        computerChoise='stump';
+    }
+// }
+// function bat(){
+    
+    let result;
+    if(computerChoise==='bat'){
+        result=`computer won`;
+    }
+    else if(computerChoise==='ball'){
+        result=`it's a tie`;
+    }
+    else if( computerChoise==='stump'){
+        result='computer won';
+    }
+
+    alert(`computer choice is ${computerChoise}. your choice is ball and ${result}`);
+}
+function stump(){
+    let randomNumber=Math.random()*3;
+    let computerChoise;  
+    if(randomNumber>0 && randomNumber<=1){
+        computerChoise='bat';
+    }
+    else if(randomNumber>1 && randomNumber<=2){
+        computerChoise='ball';
+    }
+    else if( randomNumber>2 && randomNumber<=3){
+        computerChoise='stump';
+    }
+// }
+// function bat(){
+    
+    let result;
+    if(computerChoise==='bat'){
+        result=`you won`;
+    }
+    else if(computerChoise==='ball'){
+        result='you won';
+    }
+    else if( computerChoise==='stump'){
+        result=`it's a tie`;
+    }
+
+    alert(`computer choice is ${computerChoise}. your choice is stump and ${result}`);
+}
---
+++ script/simpleCalculator.js
@@ -0,0 +1,70 @@
+let currentValue='';
+function one(){
+    currentValue=currentValue+'1';
+    document.querySelector('#input').value=currentValue;
+}
+function two(){
+    currentValue=currentValue+'2';
+    document.querySelector('#input').value=currentValue;
+}
+function three(){
+    currentValue=currentValue+'3';
+    document.querySelector('#input').value=currentValue;
+}
+function four(){
+    currentValue=currentValue+'4';
+    document.querySelector('#input').value=currentValue;
+}
+function five(){
+    currentValue=currentValue+'5';
+    document.querySelector('#input').value=currentValue;
+}
+function six(){
+    currentValue=currentValue+'6';
+    document.querySelector('#input').value=currentValue;
+}
+function seven(){
+    currentValue=currentValue+'7';
+    document.querySelector('#input').value=currentValue;
+}
+function eight(){
+    currentValue=currentValue+'8';
+    document.querySelector('#input').value=currentValue;
+}
+function nine(){
+    currentValue=currentValue+'9';
+    document.querySelector('#input').value=currentValue;
+}
+function zero(){
+    currentValue=currentValue+'0';
+    document.querySelector('#input').value=currentValue;
+}
+function dot(){
+    currentValue=currentValue+'.';
+    document.querySelector('#input').value=currentValue;
+}
+function minus(){
+    currentValue=currentValue+'-';
+    document.querySelector('#input').value=currentValue;
+}
+function plus(){
+    currentValue=currentValue+'+';
+    document.querySelector('#input').value=currentValue;
+}
+function divide(){
+    currentValue=currentValue+'/';
+    document.querySelector('#input').value=currentValue;
+}
+function multiply(){
+    currentValue=currentValue+'*';
+    document.querySelector('#input').value=currentValue;
+}
+// function clear(){
+//     currentValue='';
+//     document.querySelector('#input').value=currentValue;
+// }
+function equals(){
+    let result=eval(currentValue);
+    currentValue=result;
+    document.querySelector('#input').value=currentValue;
+}
